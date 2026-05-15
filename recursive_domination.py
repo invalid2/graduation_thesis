c81497d1e1c910ddc13dde76e5ad2bc3798138e0
fariaiago <111879008+fariaiago@users.noreply.github.com>
2026-05-14 22:22:37 -0300

    Updated RecursiveDomination

--- recursive_domination.py
+++ recursive_domination.py
@@ -1,23 +1,24 @@
 import networkx as nx
 
 def RecursiveDomination(G: nx.Graph, ddset: set, current_vertex_index: int) -> set:
-    last_vertex = list(G)[current_vertex_index - 1]
-
-    for node in set(G.neighbors(last_vertex)) | {last_vertex}:
-        neighbors = set(G.neighbors(node)) | {node}
-        dominated_by = neighbors.intersection(ddset)
-        if len(dominated_by) < 2:
-            return ddset | {last_vertex}
-    
     if current_vertex_index >= len(G.nodes()):
         return ddset
 
     vertex = list(G)[current_vertex_index]
 
     tempset = ddset.difference({vertex})
+
+    is_valid_ddset = True
+
+    for node in set(G.neighbors(vertex)) | {vertex}:
+        neighbors = set(G.neighbors(node)) | {node}
+        dominated_by = neighbors.intersection(tempset)
+        if len(dominated_by) < 2:
+            is_valid_ddset = False
+            break
     
     current_vertex_index += 1
-    solution1 = RecursiveDomination(G, tempset, current_vertex_index)
+    solution1 = RecursiveDomination(G, tempset, current_vertex_index) if is_valid_ddset else ddset
     solution2 = RecursiveDomination(G, ddset, current_vertex_index)
 
     if len(solution1) <= len(solution2):
